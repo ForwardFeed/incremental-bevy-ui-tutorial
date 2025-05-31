87bbd22fe098041bcc7403294574557e454ccd51
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-05-31 21:15:41 +0200

    made two buttons for the expoition panel

--- increment-6/src/pause_menu/exposition/root_ui.rs
+++ increment-6/src/pause_menu/exposition/root_ui.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::{relationship::RelatedSpawner, spawn::SpawnWith}, prelude::*};
 
 use super::{align_items_ui::spawn_all_align_items, justify_text_ui::spawn_text_and_border_exposition};
 
@@ -22,27 +22,93 @@ pub fn spawn_pause_menu_exposition(
         Node {
             width: Val::Percent(100.),
             height: Val::Percent(100.),
-            flex_direction: FlexDirection::Column,
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::SpaceEvenly,
             ..Default::default()
         },
         PauseMenuExpositionUiTag,
         children![ 
             (
                 Node {
-                    width: Val::Percent(80.),
+                    width: Val::Percent(90.),
                     height: Val::Percent(80.),
-                    flex_direction: FlexDirection::Column,
+                    flex_direction: FlexDirection::Row,
+                    margin: UiRect {
+                        left: Val::Percent(2.),
+                        right: Val::Percent(8.),
+                        top: Val::Percent(10.),
+                        bottom: Val::Percent(10.)
+                    },
                     ..Default::default()
                 },
                 BackgroundColor(COLOR_BG),
-                children![
-                    spawn_text_and_border_exposition(),
-                    spawn_all_align_items()
-                ]
+                Children::spawn(SpawnWith(|parent: &mut RelatedSpawner<ChildOf>|{
+                    spawn_sidebar(parent);
+                    spawn_main_content_holder(parent);
+                    /* spawn_text_and_border_exposition();
+                    spawn_all_align_items(); */
+                }))
             ),
             
         ]
     ));
+}
+
+
+fn spawn_main_content_holder(parent: &mut RelatedSpawner<ChildOf>){
+    parent.spawn(
+        (
+            Node{
+                width: Val::Percent(80.),
+                height: Val::Percent(100.),
+                ..Default::default()
+            },
+            BackgroundColor(COLOR_BG_A)
+        )
+    );
+}
+
+
+pub fn spawn_sidebar(parent: &mut RelatedSpawner<ChildOf>){
+    parent.spawn(
+        (
+            Node{
+                width: Val::Percent(20.),
+                height: Val::Percent(100.),
+                flex_direction: FlexDirection::Column,
+                ..Default::default()
+            },
+            BackgroundColor(COLOR_BG_B),
+            Children::spawn(SpawnWith(spawn_all_buttons_sidebar))
+        )
+    )
+    
+    ;
+}
+
+pub fn spawn_all_buttons_sidebar(parent: &mut RelatedSpawner<ChildOf>){
+    parent.spawn((
+        Node{
+            flex_direction: FlexDirection::Column,
+            ..Default::default()
+        },
+        children![
+            sidebar_buttons_widget("align_items"),
+            sidebar_buttons_widget("justify_text"),
+        ]
+    ));
+}
+
+pub fn sidebar_buttons_widget<T: Into<String>>(text: T) -> impl Bundle{
+    (
+        Node{
+            ..Default::default()
+        },
+        children![
+            Node{
+                ..Default::default()
+            },
+            TextLayout::new_with_justify(JustifyText::Center),
+            Text::new(text),
+        ]
+        
+    )
 }
